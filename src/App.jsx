1d6b85fa757bcbda18c93b41da2a098b931e7f7d
Maricel Lopez <mharycell@hotmail.com>
2022-09-16 20:38:25 -0500

    css

--- src/App.jsx
+++ src/App.jsx
@@ -2,53 +2,55 @@ import { useState, useEffect } from 'react'
 import Location from './components/Location';
 import axios from 'axios';
 import './App.css'
+import image from "./assets/Rectangle 1.png"
 
 function App() {
   
   const [rick, setRick] = useState({});
-  const [typeLocation, setTypeLocation] = useState([]);
-
+  const [typeLocation, setTypeLocation] = useState("");
+ 
   useEffect(() => {
     const randomId = Math.floor(Math.random() * 125) + 1
-    console.log(randomId)
+    
     axios.get(`https://rickandmortyapi.com/api/location/${randomId}`)
-      .then(res => { 
-        setRick(res.data)
-        setTypeLocation(res.data.residents)
-      });
+    .then(res => { 
+      setRick(res.data)
+    });
+    console.log(typeLocation)
   }, []);
+  
 
   const searchLocation = () => {
     axios.get(`https://rickandmortyapi.com/api/location/${typeLocation}`)
       .then(res => setRick(res.data));
   }
-
-  console.log(rick);
-
+   
   return (
-    <div className='Card'> 
+    <div className='Container'> 
       <div className="App">
-      <Location/>
+        <header>
+          <img className='image' src={image} alt="" />
+        </header>      
       <h1>{rick.name}</h1>
       <div>
         <ul className='information'>
-          <li>Type: {rick.residents?.type}</li>
+          <li>Type: {rick.type}</li>
           <li>Dimension: {rick.dimension}</li>
           <li>Population: {rick.residents?.length}</li>
         </ul>
       </div>
-      <input 
+      <input className='input' 
         type="text"
-        value={rick}
-        onChange={(e) => setRick(e.target.value)}
+        value={typeLocation}
+        onChange={(e) => setTypeLocation(e.target.value)}
       />
       <button onClick={searchLocation}>Search</button>
       </div>
       <ul className="rickAndMorty-container">
-      {typeLocation.residents?.map((resident) => (
+      {rick.residents?.map((resident) => (
        <Location url={resident} key={resident}/>
       ))}
-      </ul> 
+      </ul>     
     </div>
     
   )
--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -1,25 +1,43 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
+const Location = ({ url }) => {
+  const [data, setData] = useState({});
 
-const Location = ({url}) => {
+  useEffect(() => {
+    axios.get(url).then((res) => setData(res.data));
+  }, []);
 
-    const [data, setData] = useState({})
-
-    useEffect(() => {
-        axios.get(`${url}`)
-            .then(res => {
-                setData(res.data)
-                console.log(res.data)})           
-    })
-
-    
-    return (
-        <div>
-            <h1>{data.name}</h1>
-            <img src={data.image} alt="" />
+  return (
+    <>
+      <div className="Card">
+        <div className="Card__status">
+          <img src={data.image} alt={data.name} />
         </div>
-    );
+      </div>
+      <div className="Card__data">
+        <ul>
+          <li>
+            {" "}
+            <h2>{data?.name}</h2>{" "}
+          </li>
+          <li>
+            <h5>SPECIE: </h5>
+          </li>
+          <li>{data?.species}</li>
+          <li>
+            <h5>ORIGIN:</h5>
+          </li>
+          <li>{data.origin?.name}</li>
+          <li>
+            <h5>EPISODE:</h5>
+          </li>
+          <li>{data.episode?.length}</li>
+        </ul>
+      </div>
+      <h1>{data.name}</h1>
+    </>
+  );
 };
 
 export default Location;
